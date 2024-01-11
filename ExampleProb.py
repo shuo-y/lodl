05c944942e4c6f9b23ffac243c55a0536c06d898
Shuo Yang <yang.5229@buckeyemail.osu.edu>
2024-01-10 21:04:00 -0500

    Add a small demo for the searching weighted mse

---
+++ ExampleProb.py
@@ -0,0 +1,72 @@
+from PThenO import PThenO
+import numpy as np
+
+# Reference of https://arxiv.org/pdf/2305.16830.pdf
+# see also https://arxiv.org/pdf/2305.16830.pdf
+
+class ExampleProb(PThenO):
+    def __init__(self):
+        super(ExampleProb, self).__init__()
+        pass
+
+    def dec_loss(self, z_pred: np.ndarray, z_true: np.ndarray, verbose=False, **kwargs) -> np.ndarray:
+        choose_ind = np.argmin(z_pred, axis=1)
+        true_ind = np.argmin(z_true, axis=1)
+        zpred_dec = np.take_along_axis(z_true, np.expand_dims(choose_ind, axis=1), axis=1)
+        zopt_dec = np.take_along_axis(z_true, np.expand_dims(true_ind, axis=1), axis=1)
+        diff = (zpred_dec - zopt_dec) ** 2
+        diff = diff.sum(axis=1)
+        return diff.mean()
+
+
+    def _generate_dataset_single_fun(self, start, end, num, fun):
+        x = np.linspace(start, end, num)
+        y = np.apply_along_axis(fun, -1, np.expand_dims(x, 1))
+        return x, y
+
+    def generate_dataset_two_fun(self, start1, end1, num1, start2, end2, num2, fun1, fun2):
+        x1, y1 = self._generate_dataset_single_fun(start1, end1, num1, fun1)
+        x2, y2 = self._generate_dataset_single_fun(start2, end2, num2, fun2)
+        xy1 = np.concatenate([np.expand_dims(x1, axis=1), y1], axis=1)
+        xy2 = np.concatenate([np.expand_dims(x2, axis=1), y2], axis=1)
+
+        xy = np.concatenate([xy1, xy2], axis=0)
+        np.random.shuffle(xy)
+        return xy[:,0], xy[:, 1:]
+
+
+
+
+
+    def get_decision(self):
+        pass
+
+    def get_modelio_shape(self):
+        pass
+
+    def get_objective(self):
+        pass
+
+    def get_output_activation(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    def fun1(x):
+        return (0, 0.55)
+
+    def fun2(x):
+        return (1, 0.55)
+
+    prob = ExampleProb()
+
+    x, y = prob.generate_dataset_two_fun(0, 1, 50, 0, 1, 50, fun1, fun2)
+
+
+
+
+
+
+
+
---
+++ smalldemo.py
@@ -0,0 +1,70 @@
+import random
+import argparse
+import numpy as np
+import torch
+import xgboost as xgb
+from train_xgb import search_weights_loss
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--tree_method", type=str, default="hist", choices=["hist", "gpu_hist", "approx", "auto", "exact"])
+    parser.add_argument("--search_estimators", type=int, default=100)
+    args = parser.parse_args()
+
+    # Load problem
+    print(f"Hyperparameters: {args}\n")
+
+
+    random.seed(args.seed)
+    np.random.seed(args.seed * 1091)
+    torch.manual_seed(args.seed * 2621)
+
+    params = vars(args)
+
+    def fun1(x):
+        return (0, 0.55)
+
+    def fun2(x):
+        return (1, 0.55)
+
+    from ExampleProb import ExampleProb
+
+    prob = ExampleProb()
+
+    X, Y = prob.generate_dataset_two_fun(0, 1, 150, 0, 1, 150, fun1, fun2)
+    X = np.expand_dims(X, axis=1)
+    xtrain = X[:50]
+    ytrain = Y[:50]
+
+    xval = X[50:100]
+    yval = Y[50:100]
+
+    xtest = X[100:]
+    ytest = Y[100:]
+
+    tables = []
+
+    for w1 in range(10, 1001, 10):
+        for w2 in range(10, 1001, 10):
+            weight_vec = np.array([w1, w2])
+            cusloss = search_weights_loss(ytrain.shape[0], ytrain.shape[1], weight_vec)
+            Xy = xgb.DMatrix(xtrain, ytrain)
+
+            booster = xgb.train({"tree_method": params["tree_method"],
+                                 "num_target": 2},
+                                dtrain = Xy,
+                                num_boost_round = params["search_estimators"],
+                                obj = cusloss.get_obj_fn())
+
+            ypred = booster.inplace_predict(xtrain)
+            traindl = prob.dec_loss(ypred, ytrain)
+
+            ytestpred = booster.inplace_predict(xtest)
+            testdl = prob.dec_loss(ytestpred, ytest)
+            print(f"w1 {w1} w2 {w2} train dl{traindl} test dl{testdl}")
+            csvstring = "%.12f,%.12f,%.12f,%.12f" % (w1, w2, traindl, testdl)
+            tables.append(csvstring)
+
+    for row in tables:
+        print(row)
