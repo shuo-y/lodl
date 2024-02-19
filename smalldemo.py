29a4fe53af5ae84aa19ba58864c9256265b4c504
Shuo Yang <yang.5229@buckeyemail.osu.edu>
2024-02-19 16:19:45 -0500

    Update the small demo

--- smalldemo.py
+++ smalldemo.py
@@ -142,9 +142,9 @@ if __name__ == "__main__":
                     testdltrue = prob.dec_loss(ytest, ytest, steiner=args.steiner_degree)
                     #print(f"w1 {w1} w2 {w2} train dl{traindl} test dl{testdl}")
                     csvstring = (f"{w1:.12f},{w2:.12f},{w3:.12f},"
-                                 f"{traindl.mean():.12f}, {traindltrue.mean():.12f}, {(traindl - traindltrue).min():.12f},"
-                                 f"{valdl.mean():.12f}, {valdltrue.mean():.12f}, {(valdl - valdltrue).min():.12f},"
-                                 f"{testdl.mean():.12f}, {testdltrue.mean():.12f}, {(testdl - testdltrue).min():.12f}")
+                                 f"{traindl.mean():.12f},{traindl.std():.12f},{traindltrue.mean():.12f},{(traindl - traindltrue).min():.12f},"
+                                 f"{valdl.mean():.12f},{traindl.std():.12f},{valdltrue.mean():.12f},{(valdl - valdltrue).min():.12f},"
+                                 f"{testdl.mean():.12f},{traindl.std():.12f},{testdltrue.mean():.12f},{(testdl - testdltrue).min():.12f}")
 
 
                     tables.append(csvstring)
